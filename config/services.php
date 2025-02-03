e2d4c04421f4a04388490798b72512215b2cd4d4
Dallan Jones <dallanjones@protonmail.com>
2025-02-03 16:56:43 -0700

    Add geolocation env settings to config
    Add geolocation env settings to config

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'geolocation' => [
+        'location' => env('GEOLOCATION_LOCATION'),
+        'ip' => env('GEOLOCATION_IP'),
+    ],
+
 ];
