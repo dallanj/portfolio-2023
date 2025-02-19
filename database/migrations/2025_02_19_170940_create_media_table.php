68414d3af6421fcc3801c10d4f58a9e4a85ea677
Dallan Jones <dallanjones@protonmail.com>
2025-02-19 12:11:07 -0700

    Create migrations
    Create migrations

---
+++ database/migrations/2025_02_19_170940_create_media_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('hash')->unique();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->string('filename');
+            $table->string('path');
+            $table->string('type');
+            $table->unsignedInteger('order')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
+    }
+};
