781218b666ab5a203e9d793057d18b662d17016f
Dallan Jones <dallanjones@protonmail.com>
2025-02-25 23:06:38 -0700

    Project HTTP Controller
    Project HTTP Controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Http\Resources\ProjectResource;
+use App\Http\Resources\ProjectsCollection;
 use Inertia\Inertia;
+use App\PiniaStation\Facades\PiniaLoader;
 
 class ProjectController extends Controller
 {
@@ -13,9 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Projects/Index', [
-            'projects' => Project::with('media', 'tags', 'links')->latest()->paginate(10),
-        ]);
+        return Inertia::render('Projects/Index', []);
     }
 
     /**
