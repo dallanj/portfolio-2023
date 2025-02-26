957d77fa3fb7617d5b5f33ba335d8ba5b0754638
Dallan Jones <dallanjones@protonmail.com>
2025-02-25 23:00:55 -0700

    User loader
    User loader

---
+++ app/PiniaLoaders/UserLoader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\PiniaLoaders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserLoader
+{
+    /**
+     * Get all of the users
+     * 
+     * @return Object
+     */
+    public function all(): Object
+    {
+        return User::paginate();
+    }
+
+    /**
+     * Get all of the users
+     * 
+     * @return Object
+     */
+    public function profile(): Object
+    {
+        $user = Auth::user();
+        return $user;
+    }
+};
