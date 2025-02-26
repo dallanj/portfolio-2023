f6b262baf3a98c647959815311bf13b6358624ce
Dallan Jones <dallanjones@protonmail.com>
2025-02-25 23:06:58 -0700

    Project listing page
    Project listing page

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -1,8 +1,81 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link, router } from '@inertiajs/vue3';
+import { useProjectsStore } from '@/stores/projects';
+import { storeToRefs } from 'pinia';
+import { ref, inject, computed } from 'vue';
+import { capitalizeFirstLetter } from '@/utils/formatting';
 
-defineProps({ projects: Object });
+const { all } = storeToRefs(useProjectsStore());
+
+// Use the Pinia store
+const store = useProjectsStore();
+
+// Fix useAsyncState call
+import { useAsyncState } from '@vueuse/core';
+const { isLoading, state, isReady } = useAsyncState(
+  async () => {
+    return await store.fetchProjects();
+  },
+  {},
+  {
+    delay: 0,
+    resetOnExecute: false,
+  },
+);
+
+const { screenWidth } = inject('screenSize');
+const useCards = computed(_ => !(screenWidth.value >= 901)); // Fix this
+
+// Headers
+const headers = computed(() => {
+    if (useCards.value) {
+        return [
+            { title: 'Title', key: 'title', type: 'title', truncate: true, list: [
+                { label: 'Created', key: 'created_at', value: item => new Date(item.created_at).toDateString() },
+            ]},
+        ];
+    } else {
+        return [
+            { title: 'Title', key: 'title', width: 'w-40', truncate: true },
+            { title: 'Created', key: 'created_at', width: 'w-40', value: item => new Date(item.created_at).toDateString() },
+        ];
+    }
+});
+
+const search = () => {
+    // store.search({ ...searchParams.value });
+};
+
+const show = (item) => router.visit(item.show_url);
+
+const update = (item) => router.visit(item.update_url);
+
+const destroy = (item) => router.post(item.destroy_url);
+
+const searchParams = ref({
+    term: '',
+    type: [],
+    page: 1
+});
+
+const fetchPage = ({page, itemsPerPage, sortBy}) => {
+    searchParams.value.page = page;
+    searchParams.value.per_page = itemsPerPage;
+    searchParams.value.sortBy = sortBy;
+    search();
+};
+
+const activeClass = (active) => {
+    let classes = 'px-3 py-1 rounded-xl text-center font-bold';
+
+    classes += active
+        ? ' text-green-500 bg-green-200 border-green-600'
+        : ' text-red-500 bg-red-200 border-red-600';
+
+    return classes
+};
+
 </script>
 
 <template>
@@ -13,24 +86,36 @@ defineProps({ projects: Object });
             <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                 Projects
             </h2>
-            <Link :href="route('projects.create')">Create</Link>
+            <Link :href="all?.meta?.create_url">Create</Link>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                    <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-col gap-2">
-                        <div
-                            v-for="project in projects.data"
-                            :key="project.id"
-                            class="">
-                            <h2>{{ project.title }}</h2>
-                            <p>{{ project.description }}</p>
-                            <img v-if="project.media.length" :src="project.media[0].path" class="w-20 h-20"/>
-                            <div>
-                                <Link :href="`/projects/${project.id}`">View</Link>
-                            </div>
-                        </div>
+                    <div v-if="isReady" class="p-6 text-gray-900 dark:text-gray-100 flex flex-col gap-2">
+                        <!-- Fix SimpleDataCard -->
+                        <!-- Add is_active to projects table -->
+                        <component
+                            :is="useCards ? 'SimpleDataTable' : 'SimpleDataTable'"
+                            v-bind="{
+                                data: all,
+                                headers: headers,
+                                actions: [
+                                    { title: 'View', icon: 'file', action: item => show(item) },
+                                    { title: 'Edit', icon: 'file-pen', action: item => update(item) },
+                                    { title: 'Delete', icon: 'file-circle-xmark', action: item => destroy(item) },
+                                ],
+                                selectable: true,
+                                pagination: all,
+                                isReady: isReady,
+                            }"
+                            @fetch-page="fetchPage">
+                            <template #is_active="{ item }">
+                                <div :class="[ activeClass(item.created_at), { 'mr-4': useCards } ]">
+                                    {{ capitalizeFirstLetter(item.created_at ? 'ACTIVE' : 'NOT ACTIVE') }}
+                                </div>
+                            </template>
+                        </component>
                     </div>
                 </div>
             </div>
