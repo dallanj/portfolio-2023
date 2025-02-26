201a49a9a0b1076f4453617efc7d3278a08b7d70
Dallan Jones <dallanjones@protonmail.com>
2025-02-25 23:00:22 -0700

    Projects loader fix
    Projects loader fix

--- app/PiniaLoaders/ProjectsLoader.php
+++ app/PiniaLoaders/ProjectsLoader.php
@@ -3,12 +3,8 @@
 namespace App\PiniaLoaders;
 
 use App\Models\Project;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Arr;
-use App\Http\Resources\ProjectResource;
 use App\Http\Resources\ProjectsCollection;
-use Illuminate\Support\Facades\Log;
 
 class ProjectsLoader
 {
@@ -19,10 +15,6 @@ class ProjectsLoader
      */
     public function all()
     {
-        // return Project::all();
-        // dd(new ProjectsCollection(Project::get()));
-        return (new ProjectsCollection(Project::paginate()))->resolve(request());
-        // dd(new ProjectsCollection(Project::all()));
-        // return ProjectResource::collection(Project::all());
+        return (new ProjectsCollection(Project::paginate()))->resolve(request());    
     }
 };
