df699e54a0b14e70ec594a5d8ddcc84185ed763d
Dallan Jones <dallanjones@protonmail.com>
2025-02-25 23:05:01 -0700

    Create Project JSON Resource
    Create Project JSON Resource

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'hash' => $this->hash,
+            'title' => $this->title,
+            'description' => $this->description,
+            'overview' => $this->overview,
+            'created_at' => $this->created_at,
+            'destroy_url' => route('projects.destroy', $this),
+            'show_url' => route('projects.show', $this),
+            'update_url' => route('projects.update', $this),
+        ];
+    }
+}
