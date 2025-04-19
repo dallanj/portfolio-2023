9abf7f9ecb7e217bb1ff27ffcda99b99bdd51d7c
Dallan Jones <dallanjones@protonmail.com>
2025-04-19 16:15:10 -0600

    Resume pinia store
    Resume pinia store

---
+++ resources/js/stores/resumes.js
@@ -0,0 +1,47 @@
+import { ref } from 'vue';
+import { defineStore, acceptHMRUpdate } from 'pinia';
+import axios from 'axios';
+
+export const useResumesStore = defineStore('resumes', () => {
+    const all = ref(null);
+    const active = ref(null);
+
+    function $reset(key = null) {
+        const resetMap = {
+            all: () => { all.value = null; },
+            active: () => { active.value = null; },
+        };
+
+        if (key && resetMap[key]) {
+            // Reset only the specific key passed
+            resetMap[key]();
+        }
+    };
+
+    const actions = {
+        search: (params) => {
+            return axios.get('/api/v1/resumes', { params });
+        },
+        create: (payload) => {
+            return axios.post('/api/v1/resumes', payload);
+        },
+        update: (payload) => {
+            return axios.patch(`/api/v1/resumes/${payload.hash}`, payload);
+        },
+        destroy: (payload) => {
+            return axios.delete(`/api/v1/resumes/${payload.hash}`, payload);
+        },
+    };
+
+    return {
+        all,
+        active,
+        $reset,
+        ...actions
+    };
+});
+
+
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useResumesStore, import.meta.hot));
+}
