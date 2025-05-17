97b3d69d256ece3113edd55b2f3dae992c75f9b9
Dallan Jones <dallanjones@protonmail.com>
2025-05-17 16:43:16 -0600

    Create contacts/messages table
    Create contacts/messages table

---
+++ database/migrations/2025_05_17_095733_create_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('hash')->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->text('message');
+            $table->longText('pgp_key')->nullable();
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
