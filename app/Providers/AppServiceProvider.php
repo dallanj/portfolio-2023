8008b74e2f32028f308cf395a83ceaaf8a7c1f3f
Dallan Jones <dallanjones@protonmail.com>
2025-05-26 21:35:21 -0600

    WIP Contact Observe
    WIP Contact Observe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\PiniaLoaders\ProjectsLoader;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Contact;
+use App\Observers\ContactObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Contact::observe(ContactObserver::class);
     }
 }
